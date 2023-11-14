912438d9ed070c729360c70cfcf0b6a815ae57f4
jvalero <jvalero84@gmail.com>
2023-11-14 22:10:51 +0100

    NftMarketplace contract WIP (List and buy item)

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -5,7 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketplace__NotApprovedForMarketplace();
+error NftMarketplace__NotOwner();
 error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
 
 contract NftMarketplace {
     struct Listing {
@@ -23,6 +26,9 @@ contract NftMarketplace {
     // NFT Contract address --> NFT tokenID --> Listing
     mapping(address => mapping(uint256 => Listing)) private s_listings;
 
+    // Seller address --> Amount earned
+    mapping(address => uint256) private s_proceeds;
+
     ///////////////
     // Modifiers //
     ///////////////
@@ -38,15 +44,44 @@ contract NftMarketplace {
         _;
     }
 
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketplace__NotOwner();
+        }
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketplace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
     ////////////////////
     // Main Functions //
     ////////////////////
 
+    /*
+     * @notice Method for listing your NFT on the marketplace
+     * @param nftAddress: Address of the NFT
+     * @param tokenId: The Token ID of the NFT
+     * @param price: Sale price of the listed NFT
+     * @dev Tecnically, we could have the contract be the escrow for the NFTs
+     * but this way people can still hold their NFTs when listed.
+     */
     function listItem(
         address nftAddress,
         uint256 tokenId,
         uint256 price
-    ) external notListed(nftAddress, tokenId, msg.sender) {
+    ) external notListed(nftAddress, tokenId, msg.sender) isOwner(nftAddress, tokenId, msg.sender) {
         if (price <= 0) {
             revert NftMarketplace__PriceMustBeAboveZero();
         }
@@ -63,5 +98,25 @@ contract NftMarketplace {
 
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NftMarketplace__PriceNotMet(nftAddress, tokenId, listedItem.price);
+        }
+        s_proceeds[listedItem.seller] += msg.value;
+        // Could just send the money...
+        // https://fravoll.github.io/solidity-patterns/pull_over_push.html
+        // We need to de-list the item..
+        delete(s_listings[nftAddress][tokenId]);
+        IERC721(nftAddress).transferFrom(listedItem.seller, msg.sender, tokenId);
+
+
+        listedItem.seller = msg.sender;
+
     }
 }
